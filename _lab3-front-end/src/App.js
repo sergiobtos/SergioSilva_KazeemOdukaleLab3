4137dcf15454be942fb6f51b15d2185e74328f9a
Sergio Silva <sergiobtos@hotmail.com>
2021-03-16 15:10:54 -0400

    css fixed

--- _lab3-front-end/src/App.js
+++ _lab3-front-end/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,18 +6,20 @@ import {
   Link,
   Redirect
 } from "react-router-dom";
-import './App.css';
+
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import './App.css';
 import CreateStudent from './components/CreateStudent';
 import Home from './components/Home';
+
 
 function App() {
   return (
     <Router>
       <Navbar bg="light" expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <Navbar.Collapse id="basic=navbar-nav">
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="/home">Home</Nav.Link>
               <Nav.Link href="/createStudent">Sign Up</Nav.Link>
@@ -35,4 +37,55 @@ function App() {
   );
 }
 
+export default App;*/
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+//
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+//
+
+import CreateStudent from './components/CreateStudent';
+
+
+import Home from './components/Home';
+
+function App() {
+
+  return (
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            
+            <Nav.Link href="/create">Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    
+      <div>          
+          <Route render ={()=> < Home />} path="/home" />
+         
+          <Route render ={()=> < CreateStudent />} path="/create" />
+          
+
+      </div>
+
+    </Router>
+
+
+  );
+}
+//<Route render ={()=> < App />} path="/" />
 export default App;
+
