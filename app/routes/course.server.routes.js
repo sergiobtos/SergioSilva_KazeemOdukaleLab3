32d800eaaa89e438db024e5ac7ebbebe5558538f
Sergio <48535068+sergiobtos@users.noreply.github.com>
2021-03-21 16:56:17 -0400

    Update course.server.routes.js

--- app/routes/course.server.routes.js
+++ app/routes/course.server.routes.js
@@ -4,13 +4,12 @@ var course = require('../controllers/courses.server.controller');
 module.exports = function(app){
     app.route('/api/courses')
        .get(course.list)
-       .post(course.create);
-    //.post(student.requiresLogin, course.create);
+       .post(student.requiresLogin, course.create);
+    
 
     app.route('/api/courses/:courseId')
     .get(course.read)
-    .delete(course.delete);
-    //.delete(student.requiresLogin, course.delete);
+    .delete(student.requiresLogin, course.delete);
 
     app.param('courseId', course.courseById);
 };
