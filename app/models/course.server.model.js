3e51911647a3d056ca709db765fee41888bf9411
Sergio <48535068+sergiobtos@users.noreply.github.com>
2021-03-22 21:06:13 -0400

    Update course.server.model.js

--- app/models/course.server.model.js
+++ app/models/course.server.model.js
@@ -5,7 +5,7 @@ const CourseSchema = new Schema({
     courseCode:{ type: Number, unique: true, required: 'Course Code can not be blank'},
     courseName:{ type: String, required: 'Course Name can not be blank'},
     section:{ type: Number, required: 'Section Number is mandatory'},
-    semester:{type: Number, require: 'Semester can not be blank'},
+    semester:{type: Number, required: 'Semester can not be blank'},
     creator: {
         type: Schema.ObjectId,
         ref: 'Student'
